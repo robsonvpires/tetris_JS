74f9a90f0afb51fe3d1a99c7f5b41ec1a9f81f53
robsonvpires <robsonvpires@gmail.com>
2020-05-28 16:40:03 -0300

    Add files via upload
    adicionando features
    -MOVIMENTO DOS BLOCOS: blocos se movem para esquerda e direita e rotacionam

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 document.addEventListener('DOMContentLoaded',() =>{
 const grid = document.querySelector('.grid')
-let squares = document.querySelectorAll('.grid div')
+let squares = Array.from(document.querySelectorAll('.grid div'))
 const ScoreDisplay = document.querySelector('#score')
 const StartBtn = document.querySelector('#start-button')
 const width = 10 
 
 //Tetrominoes blocos
 const lTetromino = [
-    [1,width+1,width*2+1,2],
-    [width,width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
     [1, width+1, width*2+1, width*2],
-    [width, width*2,width*2+1,width*2+2]
-]
-const zTetromino = [
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const zTetromino = [
     [0,width,width+1,width*2+1],
-    [width+1,width+2,width*2,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
     [0,width,width+1,width*2+1],
-    [width+1,width+2,width*2,width*2+1]
-]
-const tTetromino = [
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tTetromino = [
     [1,width,width+1,width+2],
     [1,width+1,width+2,width*2+1],
     [width,width+1,width+2,width*2+1],
     [1,width,width+1,width*2+1]
-]
-const oTetromino = [
+  ]
+
+  const oTetromino = [
     [0,1,width,width+1],
     [0,1,width,width+1],
     [0,1,width,width+1],
     [0,1,width,width+1]
-]
-const iTetromino = [
+  ]
+
+  const iTetromino = [
     [1,width+1,width*2+1,width*3+1],
     [width,width+1,width+2,width+3],
     [1,width+1,width*2+1,width*3+1],
     [width,width+1,width+2,width+3]
-]
+  ]
 
-const theTetrominoes = [ lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
 let currentPosition = 4
 let currentRotation = 0
@@ -55,13 +59,27 @@ function draw(){
 
 //apaga o bloco
 function unDraw(){
-
     current.forEach(index => {
         squares[currentPosition +index].classList.remove('tetromino')
     })
 }
+//faz bloco se mover 2x por segundo
+timerId = setInterval(moveDown, 500)
 
-timerId = setInterval(moveDown, 1000)
+//direcionar funcoes para keycodes
+function control(e){
+    if(e.keyCode == 37){
+        moveLeft()
+    }else if(e.keyCode == 38){
+        rotate()
+    }else if(e.keyCode == 39){
+        moveRight()
+    }else if (e.keyCode == 40){
+        //move pra baixo
+    }
+
+}
+document.addEventListener('keyup', control)
 
 //move o bloco pra baixo
 function moveDown(){
@@ -71,14 +89,54 @@ function moveDown(){
     freeze()
 }
 
+//para o bloco no fundo do grid
 function freeze(){
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
         current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-        //começar um novo bloco a cair
+        //faz um novo bloco cair
         random = Math.floor(Math.random() * theTetrominoes.length)
         current = theTetrominoes[random][currentRotation]
         currentPosition = 4
         draw()
     }
 }
+
+//move o bloco pra esquerda <
+function moveLeft(){
+    unDraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width == 0)
+
+    if (!isAtLeftEdge) currentPosition -= 1
+
+    if(currrent.some(index => squares[currentPosition + index].classList.contains('taken'))){
+        currentPosition += 1
+    }
+
+    draw()
+}
+
+//move bloco para direita >
+function moveRight(){
+    unDraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width == width -1)
+    
+    if (!isAtRightEdge) currentPosition += 1
+
+    if(currrent.some(index => squares[currentPosition + index].classList.contains('taken'))){
+        currentPosition -= 1
+    }
+
+    draw()
+}
+//rotaciona o bloco
+function rotate(){
+    unDraw()
+    currentRotation ++
+    if(currentRotation == current.length){ //se a rotação esta em 4, volta para o 0
+        currentRotation = 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    draw()
+}
+ 
 })
